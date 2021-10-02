889454c184fa8e3476d20cd318a6014c4729940c
arshiajais <90483348+arshiajais@users.noreply.github.com>
2021-10-02 14:34:39 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+var bullet,wall;
+var speed,weight;
+var thickness;
+
+function setup() 
+{
+  createCanvas(1600,400);
+
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+
+  bullet=createSprite(50, 200, 10, 50);
+  bullet.shapeColor=color(255);
+  bullet.velocityX=speed;
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+   
+}
+
+function hascollided(Lbullet,Lwall){
+   bulletRightEdge=bullet.x +bullet.width;
+    wallLeftEdge=wall.x; 
+    if(bulletRightEdge>=wallLeftEdge)
+    { return true } return false; 
+  }
+
+function draw() 
+{
+background(0,0,0); 
+
+
+if(hascollided(wall,bullet))
+ { bullet.velocityX=0; var damage=0.5 * weight * speed* speed/thickness*thickness*thickness; 
+  if(damage>12.4) { bullet.shapeColor=color(255,0,0); text("danger",200,100); } 
+  if(damage<3.6) { bullet.shapeColor=color(0,255,0); text("safe",200,300); } }
+
+  
+   
+  drawSprites();
+}
+
+ 
